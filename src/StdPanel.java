295e4618946aa79b4140890ce86424687fbdd24a
921229 <921229@VM08.MahanAir.Aero>
2015-10-26 07:18:20 +0330

    extends jpanel and two varriables added

--- src/StdPanel.java
+++ src/StdPanel.java
@@ -1,4 +1,7 @@
-//Compeletd by Mohsen and Fateme And Mina
-public class StdPanel {
+import javax.swing.JPanel;
 
+//Compeletd by Mohsen and Fateme And Mina
+public class StdPanel extends JPanel{
+	int x = 0;
+	int y = 0;
 }
